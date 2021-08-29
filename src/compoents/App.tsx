562efca785c2bc5a2f6264cb5bb3938f89668b08
gon-zo <pnw123456@icloud.com>
2021-08-29 12:18:37 +0900

    build: webpack build #1

--- src/compoents/App.tsx
+++ src/compoents/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-// import './content/styles/App.css';
+import logo from '../content/statics/logo.svg'
 
 function App() {
     return (
         <div className="App">
-            <div>test suucess</div>
-            {/*<header className="App-header">*/}
-            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
-            {/*    <p>*/}
-            {/*        Edit <code>src/App.tsx</code> and save to reload.*/}
-            {/*    </p>*/}
-            {/*    <a*/}
-            {/*        className="App-link"*/}
-            {/*        href="https://reactjs.org"*/}
-            {/*        target="_blank"*/}
-            {/*        rel="noopener noreferrer"*/}
-            {/*    >*/}
-            {/*        Learn React*/}
-            {/*    </a>*/}
-            {/*</header>*/}
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     );
 }
