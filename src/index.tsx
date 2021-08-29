83806536cd734111991835c63eb97c3519256301
gon-zo <pnw123456@icloud.com>
2021-08-29 12:09:57 +0900

    build: webpack setting #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './content/styles/index.css';
-import App from './App';
+import App from './compoents/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
